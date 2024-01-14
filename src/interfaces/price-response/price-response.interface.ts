e87ea57f13855d1ad4e252f2bb9ddf00c287a433
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2024-01-14 02:49:37 +0100

    Swagger compatible response definitions

--- src/interfaces/price-response/price-response.interface.ts
+++ src/interfaces/price-response/price-response.interface.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Candle } from 'tradingview-ws';
 
-export class PriceData {
+/**
+ * Candle
+ */
+export class CandleData implements Candle {
   @ApiProperty({ description: 'Opening price' })
   open: number;
   @ApiProperty({ description: 'Closing price' })
@@ -9,13 +13,25 @@ export class PriceData {
   high: number;
   @ApiProperty({ description: 'Lowest price' })
   low: number;
+  @ApiProperty({ description: 'Trading volume' })
+  volume: number;
+  @ApiProperty({ description: 'Timestamp of the price information' })
+  timestamp: number;
+  @ApiProperty({ description: 'Brokerage abbreviation if applicable' })
+  broker: string;
+  @ApiProperty({ description: 'Security ticker' })
+  ticker: string;
+  @ApiProperty({ description: 'Timeframe of the resulted price' })
+  timeframe: string;
+  @ApiProperty({ description: 'Measurement name for further processing' })
+  measurement: string;
 }
 
 export class PriceResponseResult {
   @ApiProperty({ description: 'Number of returned price objects' })
   count: number;
-  @ApiProperty({ type: [PriceData], description: 'Array of the prices' })
-  prices: Array<PriceData>;
+  @ApiProperty({ type: [CandleData], description: 'Array of the prices' })
+  prices: Array<CandleData>;
 }
 
 export class PriceResponse {
